213adfdd17c747f86a494b196f0f408aed9371d6
sudhanshu <risinginfranoida@gmail.com>
2025-03-15 18:56:33 +0530

    Add

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,6 @@ export default function App({ Component, pageProps }) {
 	const router = useRouter();
 	const isAdmin = router.pathname.includes('admin');
 
-	// Jin pages pe header/footer nahi dikhana hai
 	const hideLayoutPaths = [
 		'/lpu-online-learning-platform',
 		'/amity-online-program',
