45fdccad58db5f01ea8dcae1498786ba6ed0f767
sudhanshu <risinginfranoida@gmail.com>
2025-07-23 22:30:41 +0530

    Add

--- pages/_document.js
+++ pages/_document.js
@@ -15,20 +15,21 @@ export default function Document() {
 				{/* Google Tag Manager (Head) */}
 				<script
 					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MTZVZR2W');`,
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MTZVZR2W');`,
 					}}
 				/>
+
+
+				
 			</Head>
 			<body>
 				{/* Google Tag Manager (noscript) */}
-				<noscript>
-					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-MTZVZR2W"
-						height="0"
-						width="0"
-						style={{ display: 'none', visibility: 'hidden' }}
-					></iframe>
-				</noscript>
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTZVZR2W"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				<Main />
 				<NextScript />
 			</body>
